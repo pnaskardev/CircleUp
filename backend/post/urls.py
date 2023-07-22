d94bf7b53bfbe1d55d081cca537613185d497ea3
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-22 23:05:04 +0530

    changed some views

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.feed, name='post-list'),
+    path('', views.Posts.as_view({'get': 'list'}), name='post-list'),
     path('create/', views.Posts.as_view({'post': 'create'}), name='post-create'),
-    path('profile/<str:id>/', views.Posts.as_view({'get': 'list'}), name='post-create'),
+    path('profile/<str:id>/', views.UserPosts.as_view(), name='post-create'),
 ]
--- backend/post/views.py
+++ backend/post/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics
 
 from . models import Post
 from . serializers import PostSerializer
@@ -16,12 +16,8 @@ class Posts(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
-    def list(self, request, id,*args, **kwargs):
-        user=User.objects.get(pk=id)
-        user_serializer = UserSerializer(user)
-        data = Post.objects.all().filter(created_by_id=id)
-        posts = PostSerializer(data, many=True)
-        return JsonResponse({'posts': posts.data, 'user': user_serializer.data}, safe=False)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -34,10 +30,20 @@ class Posts(viewsets.ModelViewSet):
         return JsonResponse(serializer.data)
 
 
-@api_view(['GET'])
-def feed(request):
-    list = Post.objects.all()
-    serializer = PostSerializer(list, many=True)
-    return JsonResponse(serializer.data, safe=False)
+# @api_view(['GET'])
+# def feed(request):
+#     list = Post.objects.all()
+#     serializer = PostSerializer(list, many=True)
+#     return JsonResponse(serializer.data, safe=False)
 
+class UserPosts(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [AllowAny]
 
+    def list(self, request, id, *args, **kwargs):
+        user = User.objects.get(pk=id)
+        user_serializer = UserSerializer(user)
+        data = Post.objects.all().filter(created_by_id=id)
+        posts = PostSerializer(data, many=True)
+        return JsonResponse({'posts': posts.data, 'user': user_serializer.data}, safe=False)
